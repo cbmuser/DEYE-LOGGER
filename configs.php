c7feff96127c84775b89ff653be86b700fd9582e
Michael <34414160+cbmuser@users.noreply.github.com>
2024-08-12 08:52:29 +0200

    major configs
    config the access point and mysql

---
+++ configs.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * 
+ * Deye Login
+ * 
+ */
+$Credentials = "admin:admin";
+$InverterIp  = "10.10.100.254";
+/*
+ * 
+ * MySQL Configs
+ * 
+ */
+$dbhost = "localhost";
+$dbuser = "xxxxxxxxx";
+$dbpass = "xxxxxxxxx";
+$dbase  = "solarlog";
+$dbtab  = "deye";
+?>

17b7b7c0ec9802842718e5cef0130d37d40acdff
Michael <34414160+cbmuser@users.noreply.github.com>
2024-08-12 08:53:11 +0200

    the logger script
    writes every run a dataset

---
+++ logger.php
@@ -0,0 +1,30 @@
+<?php
+
+include"configs.php";
+require"class/deye.php";
+require_once "vendor/autoload.php";
+
+$DeyeObj = new Deye();
+$DeyeObj->setCredentials($Credentials);
+$DeyeObj->setInverterIp($InverterIp);
+
+$resultset = $DeyeObj->inverterStatus();
+
+$mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbase);
+$mysqli->set_charset('utf8mb4');
+// get last id
+$query="select id from $dbtab order by id desc limit 1";
+if ($result = $mysqli->query($query)) {
+    while ($row = $result->fetch_assoc()) { $id = $row['id']; }
+}
+// convert and write values to database
+$id = intval($id); 
+$id++;
+$timestamp = time();
+$peak  = intval($resultset[5]["content"]); 
+$kwatt = floatval($resultset[6]["content"]); 
+$alarm = intval($resultset[8]["content"]);
+$sql= "insert into $dbtab (id,peak,timestamp,kwatt,alarm) VALUES ($id,$peak,$timestamp,$kwatt,$alarm)";
+$mysqli->query($sql);
+$mysqli->close();
+?>
